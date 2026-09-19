db6cf7d686c5155521d3da82bf311a1931736cd0
Mia <beltranmia@ufl.edu>
2026-09-19 00:28:30 -0400

    Updated website point system

--- src/pages/dashboard/createEventTab.jsx
+++ src/pages/dashboard/createEventTab.jsx
@@ -49,8 +49,8 @@ export default function CreateEventTab() {
 
   const eventTypeOptions = [
     { value: "gbm",              label: "GBM",              points: 1 },
-    { value: "industry_speaker", label: "Industry Speaker", points: 2 },
-    { value: "social",           label: "Social",           points: 2 },
+    { value: "industry_speaker", label: "Industry Speaker", points: 3 },
+    { value: "social",           label: "Social",           points: 1 },
     { value: "academia_speaker", label: "Academia Speaker", points: 3 },
     { value: "workshop",         label: "Workshop",         points: 3 },
     { value: "competition",      label: "Competitions",     points: 4 },
